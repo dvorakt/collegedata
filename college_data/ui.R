fcb7d64f4b007249831bbb01041404bcddf08a44
dvorakt <dvorakt@union.edu>
2017-07-17 14:17:35 -0400

    small changes

--- college_data/ui.R
+++ college_data/ui.R
@@ -1,17 +1,8 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# first cut of customizable scatterplots
 
 library(shiny)
 library(ggvis)
 
-
-# Define UI for application that draws a histogram
 shinyUI(bootstrapPage(theme="bootstrap.css",
                       shinyjs::useShinyjs(),                      
                       tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
